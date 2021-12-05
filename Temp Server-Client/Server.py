55c225e56879bade4d9acb19dbc5119c18d9785d
Darfune <72785430+Darfune@users.noreply.github.com>
2021-12-05 08:05:11 -0800

    Temp Server

---
+++ Temp Server-Client/Server.py
@@ -0,0 +1,57 @@
+
+import socket, cv2, pickle, struct, imutils
+
+# Socket Create
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print('HOST IP:', host_ip)
+port = 1234
+socket_address = (host_ip, port)
+
+# Socket Bind
+server_socket.bind(socket_address)
+
+# Socket Listen
+server_socket.listen(5)
+print("LISTENING AT:", socket_address)
+
+
+# Socket Accept
+while True:
+    client_socket, addr = server_socket.accept()
+    print('GOT CONNECTION FROM:', addr)
+    if client_socket:
+        vid = cv2.VideoCapture(0)
+        
+        while (vid.isOpened()):
+            img, frame = vid.read()
+
+        # In this part the program will use the face_recognition.py
+        # to recognice the faces that are in the cameraw view 
+        # If a face is recogniced
+
+            if face_id :
+                # Writes the person that entered the door and the time/day 
+                pass
+            else: # If the face is not recogniced then the camera video 
+                # will be viewable for the client 
+                frame = imutils.resize(frame, width=320)
+                a = pickle.dumps(frame) #serialize frame to bytes
+                message = struct.pack("Q", len(a)) + a # pack the serialized data
+                # print(message)
+                try:
+                    client_socket.sendall(message) #send message or data frames to client
+                except Exception as e:
+                    print(e)
+                    raise Exception(e)
+
+                # This part will have the face recognition
+                face_id = "ID"
+
+
+                cv2.imshow('Server', frame) # will show video frame on server side.
+
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    client_socket.close()
